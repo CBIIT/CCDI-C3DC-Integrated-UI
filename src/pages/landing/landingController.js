b4fa9ed3dad27c93907bc5c997b2ed711c0a5b64
David-YuWei <ywwei85@gmail.com>
2026-03-12 21:08:07 -0400

    Add C3DC Home page

--- src/pages/landing/landingController.js
+++ src/pages/landing/landingController.js
@@ -1,88 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import { useApolloClient } from '@apollo/client';
-import { connect } from 'react-redux';
-import env from '../../utils/env'
-import yaml from "js-yaml";
-import axios from "axios";
-import { CircularProgress } from '@material-ui/core';
-import { statsData } from '../../bento/landingPageData';
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import LandingView from './landingView';
-import { LANDING_DATA_QUERY } from '../../bento/landingPageData';
+import { Typography } from '../../components/Wrappers/Wrappers';
+import { GLOBAL_STATS_BAR_QUERY } from '../../bento/landingPageData';
 
-const CCDCurl ='https://datacatalog.ccdi.cancer.gov/service/datasets/count';
-const NEWS_URL = `${env.REACT_APP_STATIC_CONTENT_URL}/newsData.yaml`;
+const landingController = () => {
+  const { loading, error, data } = useQuery(GLOBAL_STATS_BAR_QUERY, {
+    fetchPolicy: 'no-cache',
+  });
 
-const getDashData = () => {
-  const client = useApolloClient();
-  async function getData() {
-    let result = await client.query({
-      query: LANDING_DATA_QUERY,
-      variables: {},
-    })
-      .then((response) => response.data);
-    return result;
+  if (loading) return <CircularProgress />;
+  if (error) {
+    return (
+      <Typography variant="h5" color="error" size="sm">
+        {error && `An error has occurred in loading stats component: ${error}`}
+      </Typography>
+    );
   }
 
-  async function getCCDC() {
-    const response = await fetch(CCDCurl);
-    const result = await response.json();
-    return result;
-  }
-
-  async function getNewsData() {
-    let resultData = [];
-    let result = [];
-    try {
-      const fileUrl = `${NEWS_URL}?ts=${new Date().getTime()}`;
-      result = await axios.get(
-        fileUrl
-      );
-      resultData = yaml.safeLoad(result.data);
-    } catch (_error) {
-    }
-    return resultData
-  }
-
-  const [statsDataNew, setStatsDataNew] = useState(statsData);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    getCCDC().then((result) => {
-      let newStatList = statsDataNew;
-      newStatList[0].num = result.data;
-      setStatsDataNew([...newStatList]);
-    });
-    getData().then((result) => {
-      let newStatList = statsDataNew;
-      const MCICount = result.numberOfMCICount;
-      newStatList[1].num = MCICount;
-      setStatsDataNew([...newStatList]);
-    });
-    getNewsData().then((resultData) => {
-      setData(resultData);
-    });
-    return () => controller.abort();
-  },[]);
-  return { statsDataNew, data };
+  return <LandingView statsData={formatNumbers(data)} />;
 };
 
-const LandingController = (() => {
-  const { statsDataNew, data } = getDashData();
+export default landingController;
 
-  if (!statsDataNew) {
-    return (<div style={{"height": "1200px","paddingTop": "10px"}}><div style={{"margin": "auto","display": "flex","maxWidth": "1800px"}}><CircularProgress /></div></div>);
-  }
+function formatNumbers(statCounts) {
+  const resultObject = {};
 
-  return (
-    <LandingView
-      statsData={statsDataNew}
-      newsData={data}
-    />
-  );
-});
+  Object.keys(statCounts).forEach((key) => {
+    const value = statCounts[key];
 
-const mapStateToProps = (state) => ({
-});
+    if (value >= 1000000000) {
+      const beforeDecimal = Math.floor(value / 1000000000);
+      const afterDecimal = Math.round((value % 1000000000) / 100000000) / 10;
+      resultObject[key] = { num: beforeDecimal + afterDecimal, char: 'B' };
+    } else if (value >= 1000000) {
+      const beforeDecimal = Math.floor(value / 1000000);
+      const afterDecimal = Math.round((value % 1000000) / 100000) / 10;
+      resultObject[key] = { num: beforeDecimal + afterDecimal, char: 'M' };
+    } else if (value >= 1000) {
+      const beforeDecimal = Math.floor(value / 1000);
+      const afterDecimal = Math.round((value % 1000) / 100) / 10;
+      resultObject[key] = { num: beforeDecimal + afterDecimal, char: 'K' };
+    } else {
+      resultObject[key] = { num: value, char: '' };
+    }
+  });
 
-export default connect(mapStateToProps, null)(LandingController);
+  return resultObject;
+}
